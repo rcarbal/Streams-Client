06443460f39732b97d2c0e337dd0baef79c90f5f
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-06-13 17:02:54 -0700

    Wildcard Navigation

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -8,13 +8,11 @@ class StreamList extends React.Component {
         this.props.fetchStreams();
     }
 
-    renderAdmin(stream){
-        if(stream.userId ===this.props.currentUserId){
+    renderAdmin(stream) {
+        if (stream.userId === this.props.currentUserId) {
             return (
                 <div className="right floated content">
-                    <button className="ui button primary">
-                        Edit
-                    </button>
+                    <Link to={`/streams/edit/${stream.id}`} className="ui button primary">Edit</Link>
                     <button className="ui button negative">
                         Delete
                     </button>
@@ -23,8 +21,8 @@ class StreamList extends React.Component {
         }
     }
 
-    renderList(){
-        return this.props.streams.map(stream =>{
+    renderList() {
+        return this.props.streams.map(stream => {
             return (
                 <div className="item" key={stream.id}>
                     {this.renderAdmin(stream)}
@@ -38,16 +36,16 @@ class StreamList extends React.Component {
         });
     }
 
-    renderCreate(){
-        if (this.props.isSignedIn){
-            return(
-                <div style={{textAlign: 'right'}}>
+    renderCreate() {
+        if (this.props.isSignedIn) {
+            return (
+                <div style={{ textAlign: 'right' }}>
                     <Link to="/streams/new" className="ui button primary">
                         Create Stream
                     </Link>
                 </div>
             );
-        } 
+        }
     }
 
     render() {
@@ -64,7 +62,7 @@ class StreamList extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-    return { 
+    return {
         streams: Object.values(state.streams),
         currentUserId: state.auth.userId,
         isSignedIn: state.auth.isSignedIn
